1eb412695292e4e7b1808f5ab45a2e324d5839e5
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2022-04-16 12:47:25 +0530

    src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import "jquery";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
