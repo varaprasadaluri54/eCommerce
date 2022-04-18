1157d834de5210ec1463d17a352bbef3741a8167
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2022-04-18 17:12:06 +0530

    src

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import "./styles.css";
-import { Component } from "react";
+import React, { Component } from "react";
+import MainContent from "./MainContent";
+import NavBar from "./NavBar";
+
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="/#">
-              My App
-            </a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="/#">
-                    Home
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div>
+      <>
+        <NavBar />
+        <MainContent />
+      </>
     );
   }
 }
